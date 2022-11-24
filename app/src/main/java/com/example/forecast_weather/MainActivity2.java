c5a2f6046e5a2297abb5a1cabd031541dbf0bf64
nadim50 <mdnadimmir50@gmail.com>
2022-11-24 12:04:08 +0600

    Main Page Design

--- app/src/main/java/com/example/forecast_weather/MainActivity2.java
+++ app/src/main/java/com/example/forecast_weather/MainActivity2.java
@@ -3,6 +3,7 @@ package com.example.forecast_weather;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+    }
+
+    public void getWeatherDetails(View view) {
     }
 }
