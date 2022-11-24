174b42755c198d70a3d804bf7241155163a4b640
nadim50 <mdnadimmir50@gmail.com>
2022-11-24 11:57:23 +0600

    splash_page_dynamic

--- app/src/main/java/com/example/forecast_weather/MainActivity.java
+++ app/src/main/java/com/example/forecast_weather/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.forecast_weather;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tvDisplay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvDisplay=findViewById(R.id.tvDisplay);
+
+
+        tvDisplay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent=new Intent(MainActivity.this,MainActivity2.class);
+                startActivity(myIntent);
+
+            }
+        });
     }
 }
